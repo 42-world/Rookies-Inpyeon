c3f6aab7d341c49c8aff9fcb62770a77a7a96773
chichoon <chichoon.choi@gmail.com>
2023-07-08 21:25:58 +0900

    feat: 로그인 구현
    Co-authored-by: hyeonkim <dev.hyongti@gmail.com>

---
+++ src/pages/auth/kakao/callback.tsx
@@ -0,0 +1,21 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function KakaoCallback() {
+  const {
+    push,
+    query: { code },
+  } = useRouter();
+
+  console.log(code);
+  useEffect(() => {
+    if (code) {
+      fetch(`http://localhost:8888/auth/kakao/callback?code=${code}`, {
+        method: "GET",
+      }).then((res) => {
+        push("/"); // 로그인 성공 시 메인 페이지로 이동
+      });
+    }
+  }, [code]);
+  return <div>카카오 로그인 콜백 페이지</div>;
+}
---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,12 @@
+export default function Login() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}>
+      <a
+        href="http://localhost:8888/auth/kakao"
+        className="bg-yellow-300 px-5 py-2">
+        카카오 로그인
+      </a>
+    </main>
+  );
+}
