1da8efd4ac0cbc9320dd05f2ad6dbf2c00b39239
chichoon <chichoon.choi@gmail.com>
2023-07-16 13:06:41 +0900

    fix: write 군인 및 id 존재하지 않을 때 예외처리

--- src/pages/[soldierNickname]/[displayId]/write.tsx
+++ src/pages/[soldierNickname]/[displayId]/write.tsx
@@ -11,15 +11,20 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const soldierRes = await fetch(
     `http://localhost:8888/soldier?nickname=${soldierNickname}`
   ).then((res) => res.json());
+  if (!soldierRes) return { props: { soldierNickname: null, id: null } };
+
   const linkRes = await fetch(
     `http://localhost:8888/link?soldierId=${soldierRes.id}&displayId=${displayId}`
   ).then((res) => res.json());
+  if (!soldierRes) return { props: { soldierNickname, id: null } };
+
   return {
-    props: { soldierNickname, id: linkRes.id },
+    props: { soldierNickname, id: linkRes.id ?? null },
   };
 };
 
 export default function Write({ soldierNickname, id }: Props) {
+  if (!soldierNickname || !id) return <h1>존재하지 않는 군인 또는 id입니다</h1>;
   return (
     <>
       <h1>{soldierNickname}</h1>
