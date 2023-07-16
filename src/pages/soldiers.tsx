bb69f85046baeb200ef7bbec5d4f88320eb87ac5
chichoon <chichoon.choi@gmail.com>
2023-07-16 15:25:57 +0900

    fix: 군인 목록

--- src/pages/soldiers.tsx
+++ src/pages/soldiers.tsx
@@ -1,3 +1,37 @@
-export default function Soldiers() {
-  return <div>군인들 페이지</div>;
+import { GetServerSideProps } from "next";
+import Link from "next/link";
+
+import { httpClient } from "@/services";
+import { Soldier } from "@/types/Soldier";
+
+interface Props {
+  soldiers: Soldier[];
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const soldierRes = await httpClient({
+    path: `/soldier/all`,
+  });
+  if (!soldierRes) return { props: { soldiers: null } };
+
+  console.log(soldierRes);
+  return {
+    props: { soldiers: soldierRes },
+  };
+};
+
+export default function Soldiers({ soldiers }: Props) {
+  if (!soldiers) return <h1>로그인 정보가 손상되었습니다</h1>;
+  return (
+    <div>
+      <h1>내 군인</h1>
+      {soldiers.map((soldier) => (
+        <Link
+          key={`soldier-${soldier.id}`}
+          href={`/soldiers/${soldier.nickname}`}>
+          {soldier.nickname}
+        </Link>
+      ))}
+    </div>
+  );
 }
