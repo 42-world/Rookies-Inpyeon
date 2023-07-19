5993b1893002945b04d847c64219b03ca234f13c
chichoon <chichoon.choi@gmail.com>
2023-07-20 00:21:08 +0900

    feat: 편지 열람 페이지

--- src/pages/[soldierNickname]/[displayId]/[letterId].tsx
+++ src/pages/[soldierNickname]/[displayId]/[letterId].tsx
@@ -1,20 +1,54 @@
-import { Letter } from "@/types/Letter";
 import { GetServerSideProps } from "next";
+import { getDateDistance, getDateDistanceText } from "@toss/date";
+
+import { SecondaryHeader } from "@/components";
+import { Letter } from "@/types/Letter";
+import { Text } from "@rookies-team/design";
+
+interface Props {
+  soldierNickname: string;
+  displayId: string;
+  letter: Letter;
+}
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { letterId } = context.query;
+  const { soldierNickname, displayId, letterId } = context.query;
   const res = await fetch(`http://localhost:8889/letter/${letterId}`);
   const letter = await res.json();
   return {
-    props: { letter },
+    props: { soldierNickname, displayId, letter },
   };
 };
 
-export default function Letter({ letter }: { letter: Letter }) {
+export default function Letter({ soldierNickname, displayId, letter }: Props) {
+  const distance = getDateDistance(new Date(letter.createdAt), new Date());
+  const distanceDate =
+    distance.days > 1
+      ? `${distance.days}일 전`
+      : `${distance.days * 24 + distance.hours}시간 전`;
+
   return (
-    <div>
-      <h2>{letter.title}</h2>
-      <p>{letter.content}</p>
-    </div>
+    <>
+      <SecondaryHeader
+        link={`/${soldierNickname}/${displayId}`}
+        buttonText="←"
+      />
+      <div className="py-2">
+        <Text
+          size="body1"
+          weight="medium"
+          color="secondary"
+          text={distanceDate}
+        />
+        <Text size="heading2" weight="semibold" text={letter.title} />
+      </div>
+      <Text
+        size="body1"
+        weight="regular"
+        color="secondary"
+        text={letter.content}
+        className="py-4"
+      />
+    </>
   );
 }
