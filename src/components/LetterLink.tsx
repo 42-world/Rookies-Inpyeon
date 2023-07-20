e95bb93cef88b3576cd55ec7c7ee58c116f9e9cf
chichoon <chichoon.choi@gmail.com>
2023-07-20 15:35:37 +0900

    fix: 비밀번호 창 추가

--- src/components/LetterLink.tsx
+++ src/components/LetterLink.tsx
@@ -1,10 +1,9 @@
-import { FormEvent, useState } from "react";
 import { useRouter } from "next/router";
-import { useForm } from "react-hook-form";
 import { getDateDistance } from "@toss/date";
+import { ListItem } from "@rookies-team/design";
 
 import { LetterShort } from "@/types/Letter";
-import { ListItem } from "@rookies-team/design";
+import Link from "next/link";
 
 interface Props {
   letter: LetterShort;
@@ -13,8 +12,6 @@ interface Props {
 }
 
 export const LetterLink = ({ letter, soldierNickname, displayId }: Props) => {
-  const [isActive, setIsActive] = useState(false);
-  const { register, handleSubmit, watch } = useForm();
   const router = useRouter();
 
   const distance = getDateDistance(new Date(letter.createdAt), new Date());
@@ -23,40 +20,13 @@ export const LetterLink = ({ letter, soldierNickname, displayId }: Props) => {
       ? `${distance.days}일 전`
       : `${distance.days * 24 + distance.hours}시간 전`;
 
-  function handleSubmitForm(data: any) {
-    router.push(
-      `/${soldierNickname}/${displayId}/${letter.id}?password=${data.password}`
-    );
-  }
-
-  if (letter.hasPassword) {
-    return (
-      <div className="flex flex-col">
-        <ListItem
-          onClick={() => setIsActive((prev) => !prev)}
-          secondaryTextFirst={distanceDate}
-          title={`${letter.writer}님이 보낸 인편입니다!`}
-        />
-        {isActive && (
-          <form onSubmit={handleSubmit(handleSubmitForm)}>
-            <input
-              className="border"
-              placeholder="비밀번호를 입력하세요."
-              {...register("password")}
-            />
-            <button disabled={!watch("password")}>이동</button>
-          </form>
-        )}
-      </div>
-    );
-  }
   return (
-    <ListItem
-      onClick={() =>
-        router.push(`/${soldierNickname}/${displayId}/${letter.id}`)
-      }
-      secondaryTextFirst={distanceDate}
-      title={`${letter.writer}님이 보낸 인편입니다!`}
-    />
+    <Link
+      href={`/${soldierNickname}/${displayId}/${letter.id}?password=${letter.hasPassword}`}>
+      <ListItem
+        secondaryTextFirst={distanceDate}
+        title={`${letter.writer}님이 보낸 인편입니다!`}
+      />
+    </Link>
   );
 };
